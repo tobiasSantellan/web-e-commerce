9f16d95474641d73e9e209a3ac3d2097687f3aef
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-30 22:46:19 -0300

    add bg-color to the main tag

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import PopularBikes from "@/components/PopularBikes";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#121214] h-full w-full">
       <PopularBikes />
     </main>
   );
