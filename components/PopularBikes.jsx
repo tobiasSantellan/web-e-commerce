b3c4873a1fd00ce58a8ffc65e04b3736086588e5
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-01 23:29:19 -0300

    import carousel component

--- components/PopularBikes.jsx
+++ components/PopularBikes.jsx
@@ -1,6 +1,7 @@
 import { client } from "@/app/lib/sanity";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import PopularBikeCarrousel from "./PopularBikeCarrousel";
 
 const getData = async () => {
   const query = `*[_type == "product" && references(*[_type == "category" && name == "popular" ]._id, categories)]{
@@ -27,11 +28,11 @@ const PopularBikes = async () => {
   return (
     <section className="py-24">
       <div className="container mx-auto">
-        <h2 className="text-center text-white/90">Most Popular Bikes</h2>
-        <p className="text-center mb-[30px] text-white">
+        <h2 className="text-center">Most Popular Bikes</h2>
+        <p className="text-center  mb-[30px] text-black/80">
           Top Global Premium Brands in one Destination.
         </p>
-        <div>Carousel</div>
+        <PopularBikeCarrousel bikes={bikes} />
         <Link href="/our-bikes">
           <Button className="uppercase text-center mx-auto flex justify-center items-center px-8">
             See all bikes
