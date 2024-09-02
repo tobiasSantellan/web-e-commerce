6b134593e8b52207c7e6eaa528272685f7cc4817
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-01 23:28:38 -0300

    implement PopularBikeCarrousel component, provides a reusable component showcasing a carousel of bikes, enhancing the presentation and user experience.

--- components/PopularBikeCarrousel.jsx
+++ components/PopularBikeCarrousel.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
+import Bike from "./Bike";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import Pagination from "swiper/modules";
 
-const PopularBikeCarrousel = () => {
-  return <div>PopularBikeCarrousel</div>;
+const PopularBikeCarrousel = ({ bikes }) => {
+  return (
+    <Swiper>
+      {bikes.map((bike) => (
+        <SwiperSlide className="text-white" key={bike._id}>
+          <Bike bike={bike} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default PopularBikeCarrousel;
