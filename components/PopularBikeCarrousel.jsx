158b88a9664295eea92af123d7b6f3e6879aee25
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-02 23:01:31 -0300

    introduces a responsive Swiper slider for displaying popular bikes, enhancing the user experience and adapting to different screen sizes.

--- components/PopularBikeCarrousel.jsx
+++ components/PopularBikeCarrousel.jsx
@@ -3,11 +3,25 @@ import Bike from "./Bike";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import Pagination from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 const PopularBikeCarrousel = ({ bikes }) => {
   return (
-    <Swiper>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      breakpoints={{
+        640: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        960: { slidesPerView: 3 },
+        1440: { slidesPerView: 4 },
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="popular-bike-slider mb-8"
+    >
       {bikes.map((bike) => (
         <SwiperSlide className="text-white" key={bike._id}>
           <Bike bike={bike} />
