9850b3145f66be0f5d825598a27f4f8960658423
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-04 23:04:49 -0300

    implement detailed product page layout, enhances the user experience by providing a clear and well-structured layout for displaying product details.

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -14,6 +14,7 @@ const getData = async (slug) => {
   const query = `*[_type == "product" && slug.current == "${slug}" ] [0] {
   _id,
   images,
+  name,
   price,
   price_id,
   images,
@@ -28,8 +29,62 @@ const getData = async (slug) => {
 
 const ProductDetailsPage = async ({ params }) => {
   const bikes = await getData(params.slug);
-  console.log(bikes);
-  return <div>ProductDetailsPage</div>;
+  // console.log(bikes);
+  return (
+    <section className="pt-24 pb-32 ">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-14">
+          <div className="xl:flex-1 h-[460px] bg-gray-200 xl:w-[700px] xl:h-[540px] flex justify-center items-center rounded-sm ">
+            <Image
+              src={urlFor(bikes.images[0]).url()}
+              width={473}
+              height={290}
+              priority
+              alt="bike-image"
+            />
+          </div>
+          <div className=" flex-1 flex flex-col justify-center items-start gap-10 rounded-sm">
+            <Link
+              href={"/"}
+              className="text-sm flex items-center gap-2 font-semibold"
+            >
+              <ChevronLeft size={20} />
+              Back To Home
+            </Link>
+            <div className="flex flex-col gap-6 items-start">
+              <div>
+                <h3 className="text-gray-800">{bikes.name}</h3>
+                <p className="text-red-600 text-lg font-semibold">
+                  ${bikes.price}
+                </p>
+              </div>
+              <p className=" text-sm font-light">{bikes.description}</p>
+              <AddToCartBtn
+                text="Add To Cart"
+                btnStyles=" btn-accent rounded-sm uppercase text-sm text-center p-2 text-white hover:bg-accent-hover "
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-2 items-center">
+                <PackageCheck size={20} className="text-accent" />
+                <p className="text-xs font-light">
+                  Free shipping on orders over $150
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <RefreshCw size={20} className="text-accent" />
+                <p className="text-xs font-light">Free return for 30 days</p>
+              </div>
+              <div className="flex gap-2">
+                <Clock size={20} className="text-accent" />
+                <p className="text-xs font-light">Fast delivery</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetailsPage;
