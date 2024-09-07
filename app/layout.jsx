b11c45adc3ea82fbc992d071725eb8cab91df52d
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-07 20:43:05 -0300

    Introduces the  and  components to provide a consistent layout across the application.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -14,7 +16,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
