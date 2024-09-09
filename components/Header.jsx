2b02f27e9e2292e327a8e3e362ee53e1d7e61026
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-08 21:52:12 -0300

    implement cart integration and import nav component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import CartSideBar from "./CartSideBar";
+import { CgShoppingBag } from "react-icons/cg";
+import { useShoppingCart } from "use-shopping-cart";
+
 const Header = () => {
-  return <header>Header</header>;
+  const { cartCount, handleCartClick } = useShoppingCart;
+  return (
+    <header className="bg-white shadow-lg sticky top-0 py-8 z-40">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"} className="hover:text-current">
+          <h1 className="text-[26px]">
+            <span className="text-accent">C</span>
+            YCLEZZ
+          </h1>
+        </Link>
+        <div className="flex items-center gap-[26px]">
+          <Nav />
+          <div className="relative cursor-pointer">
+            <CgShoppingBag className="text-[26px]" />
+            <div className="bg-accent w-[18px] h-[18px] absolute -right-1 -bottom-1 rounded-full text-white flex items-center justify-center text-sm font-medium">
+              3
+            </div>
+          </div>
+          <CartSideBar />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
