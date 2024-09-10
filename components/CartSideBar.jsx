5943fbf7353c049048f300e9cff925fb060ea57a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-09 22:40:11 -0300

    establishes a connection between the  and the  hook, enabling the cart sidebar to be displayed or hidden based on the current cart state.

--- components/CartSideBar.jsx
+++ components/CartSideBar.jsx
@@ -1,9 +1,17 @@
 "use client";
+import { useShoppingCart } from "use-shopping-cart";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 
 const CartSideBar = () => {
+  const {
+    cartCount,
+    cartDetails,
+    shouldDisplayCart,
+    handleCartClick,
+    totalPrice,
+  } = useShoppingCart();
   return (
-    <Sheet>
+    <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>My Shopping Cart</SheetTitle>
