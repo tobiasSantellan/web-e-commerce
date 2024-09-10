25a8164ae5bf8a06d43ef92208bfb509a32ba278
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-09 22:44:19 -0300

    add click handler to shopping cart icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import Nav from "./Nav";
 import CartSideBar from "./CartSideBar";
@@ -5,7 +6,7 @@ import { CgShoppingBag } from "react-icons/cg";
 import { useShoppingCart } from "use-shopping-cart";
 
 const Header = () => {
-  const { cartCount, handleCartClick } = useShoppingCart;
+  const { cartCount, handleCartClick } = useShoppingCart();
   return (
     <header className="bg-white shadow-lg sticky top-0 py-8 z-40">
       <div className="container mx-auto flex justify-between items-center">
@@ -17,10 +18,13 @@ const Header = () => {
         </Link>
         <div className="flex items-center gap-[26px]">
           <Nav />
-          <div className="relative cursor-pointer">
+          <div
+            onClick={() => handleCartClick()}
+            className="relative cursor-pointer"
+          >
             <CgShoppingBag className="text-[26px]" />
             <div className="bg-accent w-[18px] h-[18px] absolute -right-1 -bottom-1 rounded-full text-white flex items-center justify-center text-sm font-medium">
-              3
+              {cartCount}
             </div>
           </div>
           <CartSideBar />
