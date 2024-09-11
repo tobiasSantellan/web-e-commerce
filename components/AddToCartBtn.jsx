18171ef4dcfc32cf26592f0ba67353f36b9a7dfd
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-10 21:47:20 -0300

    enhances the user experience by providing a convenient button for adding products to the cart, incorporating essential product details for accurate cart management.

--- components/AddToCartBtn.jsx
+++ components/AddToCartBtn.jsx
@@ -1,6 +1,33 @@
-const AddToCartBtn = ({ btnStyles, text, icon }) => {
+import { useShoppingCart } from "use-shopping-cart";
+
+const AddToCartBtn = ({
+  btnStyles,
+  text,
+  icon,
+  id,
+  name,
+  currency,
+  description,
+  images,
+  price,
+}) => {
+  const { addItem } = useShoppingCart();
+
+  const bike = {
+    id: id,
+    name: name,
+    description: description,
+    images: images,
+    price: price,
+  };
+
   return (
-    <button className={`${btnStyles}`}>
+    <button
+      className={`${btnStyles}`}
+      onClick={() => {
+        addItem(bike);
+      }}
+    >
       <div>{text}</div>
       <div>{icon}</div>
     </button>
