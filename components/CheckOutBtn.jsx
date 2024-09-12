7bd10a80d82afb2bee79777577c8620ce7a519d8
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-11 23:09:05 -0300

    add checkoutbtn component

---
+++ components/CheckOutBtn.jsx
@@ -0,0 +1,9 @@
+const CheckOutBtn = () => {
+  return (
+    <button className="bg-black/80 w-full rounded-sm text-white/90 p-2 mt-2 uppercase">
+      Proceed to Checkout
+    </button>
+  );
+};
+
+export default CheckOutBtn;
