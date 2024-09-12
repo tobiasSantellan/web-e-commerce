d909960e0352f528d7e2cf183f0fe2ec01b78abf
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-11 23:08:26 -0300

    Add cart empty state and display total price with checkout button

--- components/CartSideBar.jsx
+++ components/CartSideBar.jsx
@@ -1,6 +1,9 @@
 "use client";
 import { useShoppingCart } from "use-shopping-cart";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
+import CheckOutBtn from "./CheckOutBtn";
 
 const CartSideBar = () => {
   const {
@@ -11,13 +14,40 @@ const CartSideBar = () => {
     totalPrice,
   } = useShoppingCart();
   return (
-    <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
+    <Sheet
+      open={shouldDisplayCart}
+      onOpenChange={() => handleCartClick()}
+      className="h-screen overflow-y-auto"
+    >
       <SheetContent>
         <SheetHeader>
-          <SheetTitle classname="text-left mb-12">
+          <SheetTitle className="text-left mb-8">
             My Shopping Cart({cartCount})
           </SheetTitle>
         </SheetHeader>
+        <>
+          {cartCount === 0 ? (
+            <div className="flex flex-col items-center justify-center w-full h-[500px]">
+              <h5 className="text-black/50">You Cart is Empty</h5>
+            </div>
+          ) : (
+            <ScrollArea className="h-[70vh] xl:h-[74vh] pr-4  ">
+              {cartDetails &&
+                Object.entries(cartDetails).map(([key, item]) => (
+                  <CartItem key={key} item={item} />
+                ))}
+            </ScrollArea>
+          )}
+        </>
+        {cartCount > 0 && (
+          <div>
+            <div className="flex justify-between font-semibold">
+              <div className="uppercase mb-5 lg:mb-0">Total</div>
+              <div className="">${totalPrice}</div>
+            </div>
+            <CheckOutBtn />
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   );
