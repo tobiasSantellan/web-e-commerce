c1b083b13de206f4bd91dc303e52aca3e7121f88
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-15 21:51:57 -0300

    add cancel and success url

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -7,8 +7,8 @@ const CartProvider = ({ children }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-      successUrl="succes"
-      cancelUrl="error"
+      successUrl="http://localhost:3000/stripe/success"
+      cancelUrl="http://localhost:3000/stripe/error"
       language="en-US"
       currency="USD"
       billingAddressCollection={true}
