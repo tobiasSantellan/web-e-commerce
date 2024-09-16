1df67b3547bf651e8e696b653a98473926d54969
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-15 21:52:55 -0300

    add checkout redirection logic using useShoppingCart

--- components/CheckOutBtn.jsx
+++ components/CheckOutBtn.jsx
@@ -1,6 +1,23 @@
+import { useShoppingCart } from "use-shopping-cart";
+
 const CheckOutBtn = () => {
+  const { redirectToCheckout } = useShoppingCart();
+  const handleCheckout = async () => {
+    try {
+      const res = await redirectToCheckout();
+      if (res?.error) {
+        console.log(res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <button className="bg-black/80 w-full rounded-sm text-white/90 p-2 mt-2 uppercase">
+    <button
+      className="bg-black/80 w-full rounded-sm text-white/90 p-2 mt-2 uppercase"
+      onClick={handleCheckout}
+    >
       Proceed to Checkout
     </button>
   );
