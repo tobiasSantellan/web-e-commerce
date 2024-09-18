5ef8be543fe9d8e845c05d3d85204db81cbab1a8
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-18 18:05:48 -0300

    implement BikeCategories component with filtering functionality

--- components/BikeCategories.jsx
+++ components/BikeCategories.jsx
@@ -1,5 +1,64 @@
-const BikeCategories = () => {
-  return <div>BikeCategories</div>;
+"use client";
+
+import { useEffect, useState } from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+import { Slider } from "./ui/slider";
+import Bike from "./Bike";
+const BikeCategories = ({ bikes }) => {
+  const [category, setCategory] = useState("all");
+  const [filteredBikes, setFilteredBikes] = useState([]);
+  const [price, setPrice] = useState();
+  console.log(category);
+  return (
+    <section className="min-h-[1200px] py-10 ">
+      <div className="container mx-auto">
+        <div className="flex flex-col">
+          <aside className="bg-yellow-100 w-full p-4 mb-8 xl:w-[300px] xl:h-[84vh] xl:fixed">
+            <RadioGroup
+              defaultValue="all"
+              className="flex flex-col gap-6 mb-12"
+            >
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value="all"
+                  id="all"
+                  onClick={() => setCategory("all")}
+                />
+                <label htmlFor="all">All</label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value="road"
+                  id="road"
+                  onClick={() => setCategory("road")}
+                />
+                <label htmlFor="road">Road</label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value="professional"
+                  id="professional"
+                  onClick={() => setCategory("professional")}
+                />
+                <label htmlFor="professional">Professional</label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value="extreme"
+                  id="extreme"
+                  onClick={() => setCategory("extreme")}
+                />
+                <label htmlFor="extreme">Extreme</label>
+              </div>
+            </RadioGroup>
+            {/* price slider */}
+          </aside>
+          <div className="bg-blue-300 w-full xl:w-[1050px] ml-auto">div</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BikeCategories;
