02b669476738807f490c83ee1fd5036e66ba60d0
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-17 21:46:01 -0300

    implement OurBikes component for displaying all bikes

---
+++ app/our-bikes/page.jsx
@@ -0,0 +1,27 @@
+import { client } from "@/app/lib/sanity";
+
+const getData = async () => {
+  const query = `*[_type == "product"]{
+_id,
+  name,
+  description,
+  images,
+  price,
+  price_id,
+  "slug": slug.current,
+  "categories": categories[]-> {
+  name
+  }
+}`;
+
+  const data = await client.fetch(query);
+  return data;
+};
+
+const OurBikes = async () => {
+  const bikes = await getData();
+  console.log(bikes);
+  return <div>OurBikes</div>;
+};
+
+export default OurBikes;
