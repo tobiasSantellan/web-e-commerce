2862bbcfd2dea889876fb82cc81539ad71c23c84
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-21 22:06:44 -0300

    implemen hero image and call to action

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-48 md:py-0 md:h-[820px] relative overflow-hidden bg-gray-100">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between -mt-[59px]">
+          <div className="w-full xl:max-w-[580px] md:h-[820px] flex flex-col justify-center items-start xl:ml-[50px]">
+            <h1 className="text-center xl:text-left mb-6">
+              Where Cycling <span className="text-accent">Joyful</span> Is Born
+            </h1>
+            <p className="mb-10 text-lg max-w-[508px] text-center xl:text-left font-light">
+              From beginners to experienced riders, we offer a wide range of
+              high-quality bikes, designed to suit your needs and lifestyle.
+            </p>
+            <div className="mx-auto md:mx-0">
+              <Link href="/our-bikes">
+                <Button>SHOP NOW</Button>
+              </Link>
+            </div>
+          </div>
+          <div className="hidden xl:flex mt-[-120px] mr-[50px] ">
+            <Image
+              src="/hero/hero_bike.png"
+              width={650}
+              height={480}
+              alt="hero-image"
+              quality={100}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
