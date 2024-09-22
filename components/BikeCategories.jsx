2e7840c07fd1e6b324f0dc75640098b1c34624e7
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-21 22:05:18 -0300

    Improved styles for better adaptation depending on the screen

--- components/BikeCategories.jsx
+++ components/BikeCategories.jsx
@@ -27,7 +27,7 @@ const BikeCategories = ({ bikes }) => {
     <section className="min-h-[1200px] py-10 ">
       <div className="container mx-auto">
         <div className="flex flex-col">
-          <aside className=" w-full p-4 mb-8 xl:w-[300px] xl:h-[84vh] xl:fixed xl:-ml-[100px]">
+          <aside className="w-full p-4 mb-8 xl:w-[300px] xl:h-[84vh] xl:fixed xl:ml-[10px]">
             <RadioGroup
               defaultValue="all"
               className="flex flex-col gap-6 mb-12"
@@ -67,17 +67,17 @@ const BikeCategories = ({ bikes }) => {
             </RadioGroup>
             {/* price slider */}
             <div className="max-w-56">
-              <div className="text-lg font-medium mb-4">
+              <div className="text-md font-medium mb-8">
                 Max Price:{" "}
-                <span className="text-accent font-semibold ml-2">
-                  $ {price}
-                </span>
+                <span className="text-accent font-bold ml-2">$ {price}</span>
                 <span className="ml-2">
+                  (
                   {filteredBikes.length > 1
                     ? `${filteredBikes.length} items`
                     : filteredBikes === 0
                       ? `${filteredBikes.length} items`
                       : `${filteredBikes.length} item`}
+                  )
                 </span>
                 <Slider
                   defaultValue={[900]}
@@ -88,8 +88,8 @@ const BikeCategories = ({ bikes }) => {
               </div>
             </div>
           </aside>
-          <div className="w-full xl:w-[100%] ml-[220px]">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[30px]">
+          <div className="w-full xl:w-[80%] ml-[300px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[30px] ">
               {filteredBikes.map((bike) => {
                 return <Bike bike={bike} key={bike.price_id} />;
               })}
