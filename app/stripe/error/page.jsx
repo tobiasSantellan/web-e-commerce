b452b117151a17aa8c46e784131bfd6a44d89fac
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-22 22:11:00 -0300

    create error component

--- app/stripe/error/page.jsx
+++ app/stripe/error/page.jsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <section className="py-72">
+      <div className="flex flex-col">
+        <h3 className="text-center mb-4">Something went wrong! Try again.</h3>
+        <Link href="/" className="mx-auto">
+          <Button>BACK TO HOMEPAGE</Button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
