2728803be792390caef10fe3eef41161256f00d9
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-22 22:11:46 -0300

    create success page and clear cart

--- app/stripe/success/page.jsx
+++ app/stripe/success/page.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useShoppingCart } from "use-shopping-cart";
 
 const SuccessPage = () => {
-  return <div>SuccessPage</div>;
+  const { clearCart } = useShoppingCart();
+
+  useEffect(() => {
+    const paymentSuccess = true;
+
+    if (paymentSuccess) {
+      clearCart();
+    }
+  }, [clearCart]);
+
+  return (
+    <section className="py-72">
+      <div className="flex flex-col">
+        <h3 className="text-center mb-4">
+          Your Payment Was Succesfull. <br /> Thank You!
+        </h3>
+        <Link href="/" className="mx-auto">
+          <Button>BACK TO HOMEPAGE</Button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default SuccessPage;
